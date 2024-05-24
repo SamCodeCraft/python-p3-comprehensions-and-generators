95d5a2509fbbcaaf3c1e32379e57875dffafda5c
siyajari samuel <siyajari.samuel@student.moringaschool.com>
2024-05-24 07:31:14 +0300

    List comprehesion

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
-def return_evens(num_list):
-    pass
+def return_evens(sequence):
+    return [x for x in sequence if x % 2 == 0]
+
+# Test the function
+print(return_evens([0, 1, 3, 5, 7, 8, 9]))
+
 
 def make_exclamation(sentence_list):
-    pass
+    return [sentence + "!" for sentence in sentence_list]
+
+# Test the function
+print(make_exclamation(["Hello", "I'm doing great", "Python is fun"]))
